b2fef84aeef9e3f32827e749d2aee82aedb5b3e8
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-09-01 02:02:31 -0300

    Revert "Revert "aula 04""
    This reverts commit 4bb3461453cbb1a08110b110054b8b06ce74ca91.

---
+++ 04-lists/for-forin/service.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+const URL = `https://swapi.dev/api/people`
+
+async function getPeoples(name) {
+  const url = `${URL}/?search=${name}`
+  const response = await axios.get(url)
+  return response.data
+}
+
+module.exports = {
+  getPeoples
+}
