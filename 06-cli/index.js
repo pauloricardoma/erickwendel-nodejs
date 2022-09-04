16f8753e9861bf97b1f86f7cea4fbbc24d2cd5ad
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-09-04 18:23:26 -0300

    ate aula 06

--- 06-cli/index.js
+++ 06-cli/index.js
@@ -1,9 +1,22 @@
 const Commander = require('commander')
+const Database = require('./database')
 
 async function main() {
   Commander
     .version('v1')
+    .option('-n, --nome [value]', "Nome do Heroi")
+    .option('-p, --poder [value]', "Poder do Heroi")
 
+    .option('-c, --cadastrar', "Cadastrar um hyeroi")
     .parse(process.argv)
+
+  try {
+    if(Commander.cadastrar) {
+      console.log(Commander)
+      // const result = await Database.cadastrar(Commander)
+    }
+  } catch (error) {
+    console.error('Deu ruim ', error)
+  }
 }
 main()
