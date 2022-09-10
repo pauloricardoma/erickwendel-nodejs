d1ff4b7274ce524fadfe80e31a57a54dbabd6311
Paulo Ricardo Almeida <pauloricardoma@pop-os.localdomain>
2022-09-10 01:22:45 -0300

    08 aula

--- 08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ 08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -3,11 +3,20 @@ const Postgres = require('./../db/strategies/postgres')
 const Context = require('./../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres())
+const MOCK_HEROI_CADASTRAR = { 
+  nome: 'Gavião Negro',
+  poder: 'Flexas'
+}
 
 describe('Postgres Strategy', function() {
   this.timeout(Infinity)
-  it('PostgresSQL Connection', async function () {
+  it('PostgresSQL Connection', async function() {
     const result = await context.isConnected()
     assert.equal(result, true)
+  })
+  it('cadastrar', async function() {
+    const result = await context.create(MOCK_HEROI_CADASTRAR)
+
+    assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
   })
 })
