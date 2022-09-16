be135656d56489b6f328c9389454252c97ce281f
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2022-09-16 01:56:38 -0300

    aula 14 hapi listando

---
+++ 14-hapi-listar/src/tests/apiHeroes.test.js
@@ -0,0 +1,57 @@
+const assert = require('assert')
+const api = require('./../api')
+let app = {}
+
+describe('Suite de testes da API Heroes', function() {
+  this.beforeAll(async () => {
+    app = await api
+  })
+
+  it('listar /herois', async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/herois?skip=0&limit=10'
+    })
+
+    const dados = JSON.parse(result.payload)
+    const statusCode = result.statusCode
+
+    assert.deepEqual(statusCode, 200)
+    assert.ok(Array.isArray(dados))
+  })
+  it('listar /herois - deve retornar somente 3 registros', async () => {
+    const LIMIT_LENGTH = 3
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${LIMIT_LENGTH}`
+    })
+
+    const dados = JSON.parse(result.payload)
+    const statusCode = result.statusCode
+
+    assert.deepEqual(statusCode, 200)
+    assert.ok(dados.length === LIMIT_LENGTH)
+  })
+  it('listar /herois - deve retornar um erro com limit incorreto', async () => {
+    const LIMIT_LENGTH = 'opa'
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${LIMIT_LENGTH}`
+    })
+
+    assert.deepEqual(result.payload, 'Erro interno no servidor')
+  })
+  it('listar /herois - deve filtrar um item', async () => {
+    const NAME = 'Wolverine-Sun Sep 11 2022 02:46:01 GMT-0300 (Brasilia Standard Time)'
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=1000&nome=${NAME}`
+    })
+
+    const dados = JSON.parse(result.payload)
+    const statusCode = result.statusCode
+
+    assert.deepEqual(statusCode, 200)
+    assert.ok(dados[0].nome, NAME)
+  })
+})
